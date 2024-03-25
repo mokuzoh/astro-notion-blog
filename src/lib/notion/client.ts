a1ed578a30309cf45f76bd66f0b3d1162a88f0e9
mokuzoh <161141677+mokuzoh@users.noreply.github.com>
2024-03-25 16:49:29 +0900

    Update client.ts

--- src/lib/notion/client.ts
+++ src/lib/notion/client.ts
@@ -988,7 +988,7 @@ function _buildPost(pageObject: responses.PageObject): Post {
         : '',
     FeaturedImage: featuredImage,
     Rank: prop.Rank.number ? prop.Rank.number : 0,
-    UpdateDate: prop.UpdateDate.date ? prop.UpdateDate.date.start : '空白',
+    UpdateDate: prop.Date.date ? prop.Date.date.start : ' ',
   }
 
   return post
